9181a7aa12a95e27f5486f81815b0bf2ce077479
Mikhail Lobanov <mishalob2002@gmail.com>
2022-05-19 22:41:27 +0300

    cors

--- src/services/openapi/core/OpenAPI.ts
+++ src/services/openapi/core/OpenAPI.ts
@@ -19,7 +19,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:8000',
+    BASE: 'http://51.250.31.26',
     VERSION: '0.1.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
